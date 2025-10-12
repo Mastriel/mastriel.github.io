ed88d3f3f38f09bcc194bd07d31278c22fa2ed83
Zac <zac@calathea.dev>
2025-10-12 00:06:22 -0600

    Updates

--- assets/2-22-2023-CJuCpWri.js
+++ assets/2-22-2023-CJuCpWri.js
@@ -1,6 +1,6 @@
-const e=`title=Test\r
-author=Mastriel\r
-date=2/22/2023\r
-;----;\r
-# Hi!\r
+const e=`title=Test
+author=Mastriel
+date=2/22/2023
+;----;
+# Hi!
 there's nothing to see here... <m-bounce value="yet"></m-bounce>.`;export{e as default};
---
+++ assets/5-11-2023-DV6CV6-c.js
@@ -0,0 +1,5 @@
+const t=`title=Creating Myrtus
+author=Mastriel
+date=5/11/2023
+;----;
+# Sperm`;export{t as default};
